dbb93311584dad153ba2dd0ed0497dffb52d2d6e
WDsagor <sagarwpress@gmail.com>
2022-11-06 01:12:30 +0600

    Add DB & Firebase

---
+++ controller/uploadController.js
@@ -0,0 +1,26 @@
+import { dataQuery } from "../dbConnection/db";
+
+const productImgUpload = async (req, res) => {
+  try {
+    const url = "http://" + req.headers.host;
+    const filename = req.file.filename;
+    console.log(filename);
+    const sql = "INSERT INTO allimages (imgUrl) VALUE (?)";
+    //  const getSql= `SELECT * FROM all-images WHERE img_id=${image.insertID}`
+
+    let image = await dataQuery(sql, [filename]);
+    // image = await dataQuery( getSql,[]);
+    console.log(image);
+
+    if (image.length) {
+      res.status(200).json({
+        result: image,
+        url: url + "public/images/Products-img" + filename,
+      });
+    }
+    res.status(404).json({ status: "failed", error: "Image upload faild" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+export { productImgUpload };
