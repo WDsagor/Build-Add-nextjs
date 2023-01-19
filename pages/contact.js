cbe711f1bb07f7a79d170a2295fea94a82f3866a
WDsagor <sagarwpress@gmail.com>
2023-01-20 05:23:00 +0600

    upadat delar form

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { FaFacebookSquare, FaWhatsappSquare, FaHome, FaEnvelope, FaPhoneSquareAlt } from "react-icons/fa";
+import {
+  FaFacebookSquare,
+  FaWhatsappSquare,
+  FaHome,
+  FaEnvelope,
+  FaPhoneSquareAlt,
+} from "react-icons/fa";
 import Layout from "../components/Layout";
 // import contactIMG from "/images/contactIMG.jpg";
 
@@ -14,27 +20,29 @@ const Contact = () => {
   const onSubmit = () => {};
 
   return (
-    <Layout title='Contact'>
-     
-     
-        <div
-          style={{
-            background: `url("/images/contactIMG.jpg")`,
-            backgroundSize: "cover",
-          }}
-          className=" hero min-h-screen mt-20 md:mt-16"
-        >
-          <div className="hero-overlay bg-opacity-80"></div>
-          <div className=" max-w-screen-2xl mx-auto">
+    <Layout title="Contact">
+      <div
+        style={{
+          background: `url("/images/contactIMG.jpg")`,
+          backgroundSize: "cover",
+        }}
+        className=" hero min-h-screen mt-20 md:mt-16">
+        <div className="hero-overlay bg-opacity-80"></div>
+        <div className=" max-w-screen-2xl mx-auto">
           <div className=" lg:flex justify-between text-neutral-content">
-            <div className="max-w-md justify-start align-middle px-2 lg:mr-20 lg:mt-20" data-aos="fade-right" data-aos-duration="1000">
+            <div
+              className="max-w-md justify-start align-middle px-2 lg:mr-20 lg:mt-20"
+              data-aos="fade-right"
+              data-aos-duration="1000">
               <h1 className="mb-5 text-5xl font-bold pt-10">Contact us</h1>
               <p className=" flex items-center">
                 <FaHome className="mr-2" />
                 <span>73, College Road, Matuail, Jatrabari, Dhaka-1362.</span>
               </p>
               <p>
-                <a href="mailto:buildadltd@gmail.com" className=" flex items-center">
+                <a
+                  href="mailto:buildadltd@gmail.com"
+                  className=" flex items-center">
                   <FaEnvelope className="mr-2" />
                   <span>buildadltd@gmail.com</span>
                 </a>
@@ -50,8 +58,7 @@ const Contact = () => {
                   href="https://www.facebook.com/buildingadmixture"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="mr-5 btn btn-primary text-white btn-sm"
-                >
+                  className="mr-5 btn btn-primary text-white btn-sm">
                   <FaFacebookSquare className="text-lg mr-2" />
                   <span>Facebook</span>
                 </a>
@@ -59,21 +66,27 @@ const Contact = () => {
                   href="https://wa.me/+8801673040503"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="btn btn-primary text-white btn-sm"
-                >
+                  className="btn btn-primary text-white btn-sm">
                   <FaWhatsappSquare className="text-lg mr-2" />
                   <span>Whatsapp</span>
                 </a>
               </p>
             </div>
-            <div className="max-w-lg shadow-lg shadow-white rounded-lg mt-14 p-5 mb-5" data-aos="fade-left" data-aos-duration="1000">
-              <h2 className=" text-2xl uppercase text-center text-primary">Provide your Information</h2>
-              <p className=" uppercase text-sm pb-10 text-center">We are contact as soon as posibale </p>
+            <div
+              className="max-w-lg shadow-lg shadow-white rounded-lg mt-14 p-5 mb-5"
+              data-aos="fade-left"
+              data-aos-duration="1000">
+              <h2 className=" text-2xl uppercase text-center text-primary">
+                Provide your Information
+              </h2>
+              <p className=" uppercase text-sm pb-10 text-center">
+                We are contact as soon as posibale{" "}
+              </p>
               <form onSubmit={handleSubmit(onSubmit)} className=" ">
                 <div className=" lg:grid lg:grid-flow-col lg:gap-3 ">
                   <div className="form-control">
                     <label className="label ">
-                      <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#FFF80A]">
+                      <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#ff780a]">
                         Frist name
                       </span>
                     </label>
@@ -83,15 +96,17 @@ const Contact = () => {
                       })}
                       type="text"
                       placeholder="Enter your frist name"
-                      className="input input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                      className="input input-bordered bg-transparent border-primary/50 text-white mb-3"
                     />
                     {errors.fname?.type === "required" && (
-                      <small className=" text-[#FFF80A] mt-1 self-end label-text-alt ">Frist name is required</small>
+                      <small className=" text-[#ff780a] mt-1 self-end label-text-alt ">
+                        Frist name is required
+                      </small>
                     )}
                   </div>
                   <div className="form-control">
                     <label className="label ">
-                      <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#FFF80A]">
+                      <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#ff780a]">
                         Last name
                       </span>
                     </label>
@@ -101,10 +116,12 @@ const Contact = () => {
                       })}
                       type="text"
                       placeholder="Enter your last name"
-                      className="input input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                      className="input input-bordered bg-transparent border-primary/50 text-white mb-3"
                     />
                     {errors.lname?.type === "required" && (
-                      <small className=" text-[#FFF80A] mt-1 self-end label-text-alt ">Last name is required</small>
+                      <small className=" text-[#ff780a] mt-1 self-end label-text-alt ">
+                        Last name is required
+                      </small>
                     )}
                   </div>
                 </div>
@@ -115,12 +132,12 @@ const Contact = () => {
                   <input
                     type="text"
                     placeholder="Enter your Company name"
-                    className="input input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                    className="input input-bordered bg-transparent border-primary/50 text-white mb-3"
                   />
                 </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text  text-white after:content-['*'] after:ml-0.5 after:text-[#FFF80A]">
+                    <span className="label-text  text-white after:content-['*'] after:ml-0.5 after:text-[#ff780a]">
                       Email
                     </span>
                   </label>
@@ -131,19 +148,23 @@ const Contact = () => {
                     })}
                     type="email"
                     placeholder="Your email"
-                    className="input input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                    className="input input-bordered bg-transparent border-primary/50 text-white mb-3"
                     autoComplete="off"
                   />
                   {errors.email?.type === "required" && (
-                    <small className=" text-[#FFF80A] mt-1 self-end label-text-alt">Email is required</small>
+                    <small className=" text-[#ff780a] mt-1 self-end label-text-alt">
+                      Email is required
+                    </small>
                   )}
                   {errors.email?.type === "pattern" && (
-                    <small className=" text-[#FFF80A] mt-1 self-end label-text-alt">Your email is invalid</small>
+                    <small className=" text-[#ff780a] mt-1 self-end label-text-alt">
+                      Your email is invalid
+                    </small>
                   )}
                 </div>
                 <div className="form-control">
                   <label className="label ">
-                    <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#FFF80A]">
+                    <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#ff780a]">
                       Phone number
                     </span>
                   </label>
@@ -153,17 +174,17 @@ const Contact = () => {
                     })}
                     type="number"
                     placeholder="+880 1700 000000"
-                    className="input input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                    className="input input-bordered bg-transparent border-primary/50 text-white mb-3"
                   />
                   {errors.phone?.type === "required" && (
-                    <small className=" text-[#FFF80A] mt-1 self-end label-text-alt ">
+                    <small className=" text-[#ff780a] mt-1 self-end label-text-alt ">
                       Please enter your phone number
                     </small>
                   )}
                 </div>
                 <div className="form-control">
                   <label className="label ">
-                    <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#FFF80A]">
+                    <span className="label-text text-white after:content-['*'] after:ml-0.5 after:text-[#ff780a]">
                       Massages
                     </span>
                   </label>
@@ -173,21 +194,26 @@ const Contact = () => {
                     })}
                     type="textarea"
                     placeholder="Type here your massages"
-                    className="input h-36 input-bordered bg-transparent border-yellow-300 text-white mb-3"
+                    className="input h-36 input-bordered bg-transparent border-primary/50 text-white mb-3"
                   />
                   {errors.massages?.type === "required" && (
-                    <small className=" text-[#FFF80A] mt-1 self-end label-text-alt ">Write your massages</small>
+                    <small className=" text-[#ff780a] mt-1 self-end label-text-alt ">
+                      Write your massages
+                    </small>
                   )}
                 </div>
                 <div className="form-control mt-6">
-                  <input type="submit" value="submit" className="btn btn-primary text-white" />
+                  <input
+                    type="submit"
+                    value="submit"
+                    className="btn btn-primary text-white"
+                  />
                 </div>
               </form>
             </div>
           </div>
-          </div>
         </div>
-      
+      </div>
     </Layout>
   );
 };
--- pages/dashboard/addDealer.js
+++ pages/dashboard/addDealer.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { useForm, useWatch } from "react-hook-form";
+import { useFieldArray, useForm, useWatch } from "react-hook-form";
 import DashboardLayout from "../../components/DashboardLayout";
+import { FiTrash } from "react-icons/fi";
+import { rule } from "postcss";
 
 const AddDealer = () => {
   const {
@@ -10,7 +12,15 @@ const AddDealer = () => {
     formState: { errors },
   } = useForm({ mode: "onTouched" });
   const term = useWatch({ control, name: "term" });
-
+  const {
+    fields: resFields,
+    append: resAppend,
+    remove: resRemove,
+  } = useFieldArray({
+    control,
+    name: "addMore",
+  });
+  console.log(errors);
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -141,6 +151,45 @@ const AddDealer = () => {
                 )}
               </div>
             </div>
+            <div className="mx-auto lg:mx-0 max-w-md lg:max-w-full">
+              <label className="label font-bold">
+                <span className="label-text after:content-['*'] after:ml-0.5 after:text-red-500">
+                  Add More Fields
+                </span>
+              </label>
+              {resFields.map((item, index) => {
+                return (
+                  <div key={index} className="flex items-end gap-3 w-full">
+                    <div className=" form-control w-full max-w-md p-1">
+                      <input
+                        {...register(`addMore[${index}]`)}
+                        type="text"
+                        placeholder="Contact Person Mobile no"
+                        className="input input-bordered"
+                      />
+                    </div>
+                    <button
+                      type="button"
+                      onClick={() => resRemove(index)}
+                      className="grid mb-2 place-items-center rounded-full bg-red-500/20 border border-red-500 h-11 w-11 group transition-all hover:bg-red-500">
+                      <FiTrash
+                        className="text-red-500 group-hover:text-white transition-all"
+                        size="20"
+                      />
+                    </button>
+                  </div>
+                );
+              })}
+              <div>
+                <button
+                  type="button"
+                  onClick={() => resAppend("")}
+                  className="btn btn-sm mt-2 ">
+                  Add More
+                </button>
+              </div>
+            </div>
+
             <div className=" flex flex-col items-center content-center">
               <label className="label lg:self-start">
                 <span className="label-text after:content-['*'] after:ml-0.5 after:text-red-500">
@@ -148,10 +197,17 @@ const AddDealer = () => {
                 </span>
               </label>
               <textarea
+                {...register("details", {
+                  required: true,
+                })}
                 className="textarea max-w-md lg:max-w-full w-full resize h-28"
                 placeholder="Details provide your area, zone"></textarea>
+              {errors?.details?.type === "required" && (
+                <small className="  text-red-500 mt-1 self-end label-text-alt ">
+                  Please provide details your area, zone
+                </small>
+              )}
             </div>
-
             <div className="flex justify-between items-center w-full mt-3">
               <div className="flex  w-full max-w-xs">
                 <input
